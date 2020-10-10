cf46fb60e43e0b388f7409d3e191fc7a88db25cc
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-10-10 17:53:55 +0400

    Update strings

--- 04_strings.php
+++ 04_strings.php
@@ -1,8 +1,8 @@
 <?php
 
-// 1. Create simple string
-$string = "Hello TheCodeholic"; // 2. Extract TheCodeholic into variable
-$string2 = 'Hello TheCodeholic'; // 3. Replace TheCodeholic with variable
+// Create simple string
+$string = "Hello, I am Zura"; // Extract TheCodeholic into variable
+$string2 = 'Hello, I am Zura'; // Replace TheCodeholic with variable
 echo $string . '<br>';
 echo $string2 . '<br>';
 
@@ -34,27 +34,5 @@ $invoiceNumber2 = 123456;
 echo str_pad($invoiceNumber, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
 echo str_pad($invoiceNumber2, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
 echo str_repeat('Hello', 2) . '<br>' . PHP_EOL;
-
-// 6. Multiline text and line breaks
-$longText = "
-  Hello, my name is Zura
-  I am 27,
-  I love my daughter
-";
-echo $longText . '<br>' . PHP_EOL;
-echo nl2br($longText) . '<br>' . PHP_EOL;
-
-// 7. Multiline text and reserve html tags
-$longText = "
-  Hello, my name is <b>Zura</b>
-  I am <b>27</b>,
-  I love my daughter
-";
-echo "1 - " . $longText . '<br>';
-echo "2 - " . nl2br($longText) . '<br>';
-echo "3 - " . htmlentities($longText) . '<br>' . PHP_EOL;
-echo "4 - " . nl2br(htmlentities($longText)) . '<br>' . PHP_EOL;
-echo "5 - " . html_entity_decode(htmlentities($longText)) . '<br>' . PHP_EOL;
-echo "6 - " . htmlspecialchars($longText) . '<br>' . PHP_EOL;
 
 // https://www.php.net/manual/en/ref.strings.php
