d7206bfc993ecf433c36a12cb14c0d8da854b394
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-10-11 12:35:57 +0400

    Update conditionals

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -3,72 +3,51 @@
 $age = 20;
 $salary = 300000;
 
-//// 1. if condition
-//if ($age < 22) {       // 1.1 Without curly braces { }
-//    echo 'You are young man!!<br>';
-//}
-//
-//// 2. if condition - else
-//if ($age < 22) {
-//    echo 'You are young man!!<br>';
-//} else {
-//    echo 'You are not young man!!<br>';
-//}
-//
-//// 3. if condition1 AND condition2
-//if ($age < 22 && $salary > 500000) {
-//    echo 'You are young AND rich<br>';
-//}
-//
-//// 4. if condition1 OR condition2
-//if ($age < 22 || $salary > 500000) {
-//    echo 'You are young OR rich<br>';
-//}
-//
-//// 5. if condition1 - elseif condition2 - else
-//if ($age < 22) {
-//    echo 'You are young man!!<br>';
-//} elseif ($age > 60) {
-//    echo 'You are old<br>';
-//} else {
-//    echo "You are not young, but not old also<br>";
-//}
-//// 6. if condition1 and condition2 - elseif condition1 and condition2 - else
-//if ($age < 22 && $salary >= 500000) {
-//    echo 'You are young man AND rich!!<br>';
-//} elseif ($age < 22 && $salary < 500000) {
-//    echo "You are young, and not so rich<br>";
-//} elseif ($age > 60 && $salary >= 500000) {
-//    echo 'You are old, but rich<br>';
-//} elseif ($age > 60 && $salary < 500000) {
-//    echo 'You are old and NOT rich also<br>';
-//}
+// Sample if
+if ($age == 20) {
+    echo "1";
+}
 
-// 7. Ternary if
+// Without circle braces
+if ($age === 20)
+    echo "1";
+
+// Sample if-else
+if ($age > 20) {
+    echo "1";
+} else {
+    echo "2";
+}
+
+// Difference between == and ===
+$age == 20; // true
+$age == '20'; // true
+
+$age === 20; // true
+$age === '20'; // false
+
+// if AND
+if ($age > 20 && $salary === 300000) {
+
+}
+// if OR
+if ($age > 20 OR $salary === 300000) {
+
+}
+
+// Ternary if
 echo $age < 22 ? 'Young' : 'Old';
 echo '<br>';
-// 7.1 Nested ternary
-echo $age < 22 ? ($age < 16 ? 'Too young' : 'Young') : 'Old';
-echo '<br>';
-// 7.2 Short ternary
+
+// Short ternary
 $myAge = $age ?: 18; // Equivalent of "$age ? $age : 18"
 
-// 8. Null coalescing operator
+// Null coalescing operator
 $var = isset($name) ? $name : 'John';
 $var = $name ?? 'John'; // Equivalent of above
 echo $var.'<br>';
 
-// 9. Null coalescing assignment operator. Since PHP 7.4
-$person = [
-    'name' => 'John' // 9.1 Comment this line
-];
-if (!isset($person['name'])){
-    $person['name'] = 'Anonymous';
-}
-$person['name'] ??= 'Anonymous';
-echo $person['name'].'<br>';
-
-// 10. switch
+// switch
 $userRole = 'admin'; // admin, editor, user
 
 switch ($userRole) {
