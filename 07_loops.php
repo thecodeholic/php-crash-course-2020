792ef148634af442d868c6ab1141038a6e115ccf
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-10-11 12:37:42 +0400

    Update loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,42 +1,37 @@
 <?php
 
-// 1. while
+// while
 while (true) { // Infinite loop: DON'T run this
     // Do something constantly
 }
 
-// 2. Loop with $counter
-// 2.2. Add $runLoop variable to above loop
+// Loop with $counter
 $counter = 0; // When counter is 10??
 while ($counter < 10) {
-    echo "Printing counter: $counter<br>";
-    // if ($counter > 5) break; // 2.1. BREAK
-    // 2.3. Set $runLoop into false when $counter > 10
-    /*if ($counter > 10) {
-        $runLoop = false;
-    }*/
+    echo $counter.'<br>';
+    // if ($counter > 5) break;
     $counter++;
 }
 
-// 3. do - while
+// do - while
 $counter = 0; // When counter is 10?
 do {
     // Do some code right here
     $counter++;
 } while ($counter < 10);
 
-// 4. for
+// for
 for ($i = 0; $i < 10; $i++) {
-    echo "Printing counter: $i<br>";
+    echo $i."<br>";
 }
 
-// 5. foreach
+// foreach
 $fruits = ["Banana", "Apple", "Orange"];
 foreach ($fruits as $i => $fruit) {
     echo $i . ' ' . $fruit . '<br>';
 }
 
-// 6. Iterate Over associative array.
+// Iterate Over associative array.
 $person = [
     'name' => 'Brad',
     'surname' => 'Traversy',
@@ -44,7 +39,7 @@ $person = [
     'hobbies' => ['Tennis', 'Video Games'],
 ];
 foreach ($person as $key => $value) {
-    if ($key === 'hobbies') { // 8. Break when $key = 'hobbies'
+    if ($key === 'hobbies') {
         break;
     }
     echo $key . ' ' . $value . '<br>';
