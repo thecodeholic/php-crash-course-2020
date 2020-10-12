5a2736adfa8534effb3bc89e29d8a757e90dd068
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-10-12 07:41:51 +0400

    Update strings add multiline text

--- 04_strings.php
+++ 04_strings.php
@@ -35,4 +35,28 @@ echo str_pad($invoiceNumber, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
 echo str_pad($invoiceNumber2, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
 echo str_repeat('Hello', 2) . '<br>' . PHP_EOL;
 
+
+// Multiline text and line breaks
+$longText = "
+  Hello, my name is Zura
+  I am 27,
+  I love my daughter
+";
+echo $longText . '<br>' . PHP_EOL;
+echo nl2br($longText) . '<br>' . PHP_EOL;
+
+// Multiline text and reserve html tags
+$longText = "
+  Hello, my name is <b>Zura</b>
+  I am <b>27</b>,
+  I love my daughter
+";
+echo "1 - " . $longText . '<br>';
+echo "2 - " . nl2br($longText) . '<br>';
+echo "3 - " . htmlentities($longText) . '<br>' . PHP_EOL;
+echo "4 - " . nl2br(htmlentities($longText)) . '<br>' . PHP_EOL;
+echo "5 - " . html_entity_decode(htmlentities($longText)) . '<br>' . PHP_EOL;
+echo "6 - " . htmlspecialchars($longText) . '<br>' . PHP_EOL;
+
+
 // https://www.php.net/manual/en/ref.strings.php
