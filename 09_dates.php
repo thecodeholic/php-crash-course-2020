c1fa791392088b9440dddb9ef26dd0ad5a68af9b
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-10-12 07:37:58 +0400

    Update dates

--- 09_dates.php
+++ 09_dates.php
@@ -1,16 +1,13 @@
 <?php
 
-// Print current timestamp
-
 // Print current date
 
 // Print yesterday
 
 // Different format: https://www.php.net/manual/en/function.date.php
 
-// String to timestamp
+// Print current timestamp
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
 
-// Parse date from format
-// https://www.php.net/manual/en/function.date-parse-from-format.php
+// Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
