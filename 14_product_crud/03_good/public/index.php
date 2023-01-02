68816c324f11cff778db0fa76a4fa40be7633ea6
Yagya Adhikari <60005812+Yagya-Adh@users.noreply.github.com>
2023-01-02 20:06:04 +0545

    Update index.php

--- 14_product_crud/03_good/public/index.php
+++ 14_product_crud/03_good/public/index.php
@@ -8,13 +8,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 $database = new \app\Database();
 $router = new Router($database);
 
-$router->get('/', [ProductController::class, 'index']);
-$router->get('/products', [ProductController::class, 'index']);
-$router->get('/products/index', [ProductController::class, 'index']);
-$router->get('/products/create', [ProductController::class, 'create']);
-$router->post('/products/create', [ProductController::class, 'create']);
-$router->get('/products/update', [ProductController::class, 'update']);
-$router->post('/products/update', [ProductController::class, 'update']);
-$router->post('/products/delete', [ProductController::class, 'delete']);
+$router->get('/', [new ProductController(), 'index']);
+$router->get('/products', [new ProductController(), 'index']);
+$router->get('/products/index', [new ProductController(), 'index']);
+$router->get('/products/create', [new ProductController(), 'create']);
+$router->post('/products/create', [new ProductController(), 'create']);
+$router->get('/products/update', [new ProductController(), 'update']);
+$router->post('/products/update', [new ProductController(), 'update']);
+$router->post('/products/delete', [new ProductController(), 'delete']);
 
 $router->resolve();
