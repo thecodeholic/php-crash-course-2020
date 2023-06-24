3caad5f344889c48dd1df53f1ed0be3bd1b1ca9d
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-06-24 20:15:11 +0200

    Add files via upload

---
+++ more-concatenation.php/more-concatenation.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstSentence = "The quick brown fox jumped over the lazy dog. However, the dog was not happy with the fox at all and started to bark at the fox.";
+
+$secondSentence = "The quick brown fox jumped ";
+$secondSentence .= "over the lazy dog. ";
+$secondSentence .= "However, the dog was not happy with the fox ";
+$secondSentence .= "at all and started to bark at the fox.";
+
+echo $firstSentence;
+
+echo "<br>";
+
+echo $secondSentence;
