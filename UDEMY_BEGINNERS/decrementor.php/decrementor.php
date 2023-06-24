d16a45a388e1b591a18ff9233fb0a85ef4da3114
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-06-24 20:33:14 +0200

    Add files via upload

---
+++ UDEMY_BEGINNERS/decrementor.php/decrementor.php
@@ -0,0 +1,19 @@
+<?php
+
+$i = 0;
+
+echo $i;
+
+echo "<br>";
+
+$i = $i - 1;
+
+echo $i . "<br>";
+
+$i -= 1;
+
+echo $i . "<br>";
+
+$i--;
+
+echo $i . "<br>";
---
+++ UDEMY_BEGINNERS/logical.php/logical.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 10;
+
+$b = 5;
+
+$c = TRUE;
+
+$d = FALSE;
+
+if($d || ($a == 10 && $b > 10)){
+    echo "True";
+} else {
+    echo "False";
+}
