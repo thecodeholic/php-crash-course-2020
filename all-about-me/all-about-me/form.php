db07eef3f6b75bb4a28953949fd00933648e1bdc
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-06-25 17:12:55 +0200

    cookies

---
+++ all-about-me/all-about-me/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All About Me</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <input type="text" name="name">
+        <input type="number" name="age">
+        <input type="text" name="colour">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
---
+++ all-about-me/all-about-me/retrieve.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_COOKIE['name'];
+$age = $_COOKIE['age'];
+$colour = $_COOKIE['colour'];
+
+echo "Your name is $name<br>";
+
+echo "Your age is $age<br>";
+
+echo "Your favourite colour is $colour";
